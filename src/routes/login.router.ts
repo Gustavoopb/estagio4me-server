874efdf4e952f190ecab0ba7a085f34f0a3b796d
Gustavo Barbosa <guspb94@gmail.com>
2017-03-17 19:29:04 -0300

    Login and Register endpoints

--- src/routes/login.router.ts
+++ src/routes/login.router.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
 import { AbstractRouter } from './abstract/abstract.router'
 import { User } from '../schema/user.schema'
+import { IUserModel } from '../model/user.model'
+import { ServerConfig } from '../config/server.config'
 import * as passport from 'passport'
+import * as jwt from 'jwt-simple'
 
 
 class UserRoute extends AbstractRouter {
@@ -15,19 +18,40 @@ class UserRoute extends AbstractRouter {
         User.register(new User(req.body.user), req.body.password, function (err, account) {
             if (err) {
                 console.log('error while user register!', err)
-                res.json(err)
-                return next(err)
+                res.status(500).json(err)
             }
-            res.status(200).json(account)
+            var result = {
+                [account]: account,
+                message: "You have been successful registred!"
+            }
+            res.status(200).json(result)
+        })
+    }
+
+    public checkEmailUsername(req: Request, res: Response, next: NextFunction) {
+        User.findOne(req.body, function (err, result) {
+            if (err) {
+                res.status(500).json(err)
+            }
+            res.status(200).json(result)
         })
     }
 
     public login(req: Request, res: Response, next: NextFunction) {
-        res.status(200).json(req.user)
+        var user: IUserModel = new User(req.user)
+        var token = { token: jwt.encode(user, ServerConfig.jwtSecret) }
+        var result = {
+            token,
+            message: "You have been successful registred!"
+        }
+        req.originalUrl
+        console.log(token, req.headers["origin"])
+        res.status(200).json(result)
     }
 
     init() {
         this.router.post("/singUp", this.singUp)
+        this.router.post("/checkEmailUsername", this.checkEmailUsername)
         this.router.post("/login", passport.authenticate('local'), this.login)
         super.beUsed()
     }
